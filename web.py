848842974bb119d027b6e774d98af596113c1d8d
mike <michele.rullo90@gmail.com>
2016-10-08 17:40:29 +0200

    [+] basic input validation

--- web.py
+++ web.py
@@ -16,9 +16,17 @@ def submit():
     #Alternative is to hard-code request MIME type, which is the "good" way...
     request.get_data()
     port = request.form['port_input']
+
+    portInt = 0
+
+    try:
+        portInt = int(port)
+    except:
+        return render_template('input_page.html')
+
     explain = "is the port number for"
-    result = ports.search_port(port)
-    return render_template('input_page.html', port=port, result=result[1], explain=explain)
-    
+    result = ports.search_port(portInt)
+    return render_template('input_page.html', port=portInt, result=result[1], explain=explain)
+
 if __name__ == "__main__":
     app.run()
